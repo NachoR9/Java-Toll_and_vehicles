11f604f74a7612214c4731e6e5f6731d3af9fd42
NachoR9 <nachorm9@hotmail.com>
2024-09-03 20:04:39 -0700

    toll station class created

---
+++ src/main/java/dev/nacho/TollStation.java
@@ -0,0 +1,16 @@
+package dev.nacho;
+
+public class TollStation {
+    private String name;
+    private String city;
+    private int tollCollected;
+
+
+    public TollStation(String name, String city, int tollCollected) {
+        this.name = name;
+        this.city = city;
+        this.tollCollected = tollCollected;
+    }
+
+    
+}
